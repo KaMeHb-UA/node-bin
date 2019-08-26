75d2ed222fc725fcb0710223e53cfc3de2c3a0b6
KaMeHb-UA <marlock@etlgr.com>
2019-08-26 21:26:36 +0300

    initial downloader

--- downloader.js
+++ downloader.js
@@ -1,2 +1,15 @@
-var platform = require('./platform-definition');
-console.log(platform)
+var version = require('./package.json').version,
+    platform = require('./platform-definition')(),
+    linkGenerator = require('./compileLink'),
+    https = require('https'),
+    link = linkGenerator(version, platform),
+    format = link.split('.').pop(),
+    Extractor = require(format === 'xz' ? format = 'tar.xz' : format === 'gz' ? format = 'tar.gz' : format);
+
+module.exports = function(dir){
+    https.request(link, res => {
+        const extractor = new Extractor(dir);
+        res.pipe(extractor);
+        res.on('end', () => { extractor.finish() });
+    })
+}
